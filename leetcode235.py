cd1e0699fe4d644716fc82bc65aa4be908561dcd
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2025-01-24 20:49:34 -0500

    Update leetcode235.py

--- leetcode235.py
+++ leetcode235.py
@@ -8,6 +8,13 @@ class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left,p,q)
+            # We are thinking about this as if we are reading the tree from left to right. 
+        # Left node -> Root -> Right node / Left Subtree to Right Subtree
+        # p -> root 
+        # root -> q
+        # Or 
+        # q -> root 
+        # root -> p
         elif root.val < p.val and root.val < q.val:
             return self.lowestCommonAncestor(root.right,p,q)
         else:
