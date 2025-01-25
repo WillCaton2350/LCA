f317e58bd1b44144b35546e70b54f5544f0b1286
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2025-01-24 20:43:05 -0500

    Create leetcode235.py

---
+++ leetcode235.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
+        elif root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        else:
+            return root
+        # This is the recursive solution the function calls its self with smaller sub problems until a base case is met. Base Case == return root / sub problems == if and elif conditions
